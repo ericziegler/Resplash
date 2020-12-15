a6e0583f78bef67ab3f1e305f2c984ce7d7e70f8
Eric <eric@zigabytes.com>
2020-12-14 22:02:50 -0500

    EZ: Create data models for LogManager, Log, and Beverage.

---
+++ Resplash/Data/Beverage.swift
@@ -0,0 +1,8 @@
+//
+//  Beverage.swift
+//  Resplash
+//
+//  Created by Eric Ziegler on 12/14/20.
+//
+
+import Foundation
---
+++ Resplash/Data/Log.swift
@@ -0,0 +1,8 @@
+//
+//  Log.swift
+//  Resplash
+//
+//  Created by Eric Ziegler on 12/14/20.
+//
+
+import Foundation
